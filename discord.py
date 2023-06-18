ca8047f5b7f6926ca2f8050cde721735fb9c7407
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-06-18 11:27:03 -0500

    added role delete feature

--- discord.py
+++ discord.py
@@ -12,47 +12,44 @@ dotenv.load_dotenv()
 bot = lightbulb.BotApp(token=os.environ['DISCORD_TOKEN'])
 
 # Define a new custom View that contains 3 items
-class BasicView(miru.View):
-
+class FieldView(miru.View):
     # Define a new Button 
     @miru.button(label="🖥️ Software Engineer", style=hikari.ButtonStyle.PRIMARY)
     async def basic_button1(self, button: miru.Button, ctx: miru.ViewContext) -> None:
-        user = ctx.author
-        userId = ctx.author.id
-        guildId = ctx.guild_id
-        roleId = 1117591569495752754
-        member = await bot.rest.fetch_member(guildId, userId)
-        await member.add_role(roleId)
-        print(f"{user} chose software engineer!")
-    # Define a new Button 
+        await handle_role(ctx, 1117591569495752754, "Software Engineer")
     
     @miru.button(label="🔧 Mechanical Engineer", style=hikari.ButtonStyle.PRIMARY)
     async def basic_button2(self, button: miru.Button, ctx: miru.ViewContext) -> None:
-        user = ctx.author
-        userId = ctx.author.id
-        guildId = ctx.guild_id
-        roleId = 1117591640601800726
-        member = await bot.rest.fetch_member(guildId, userId)
-        await member.add_role(roleId)
-        print(f"{user} chose mechanical engineer!")
+        await handle_role(ctx, 1117591640601800726, "Mechanical Engineer")
     
     @miru.button(label="🖌️ Concept Artist", style=hikari.ButtonStyle.PRIMARY)
     async def basic_button4(self, button: miru.Button, ctx: miru.ViewContext) -> None:
-        user = ctx.author
-        userId = ctx.author.id
-        guildId = ctx.guild_id
-        roleId = 1117591718817189928
-        member = await bot.rest.fetch_member(guildId, userId)
-        await member.add_role(roleId)
-        print(f"{user} chose Concept Artist!")
+        await handle_role(ctx, 1117591718817189928, "Concept Artist")
+
+
+class LocationView(miru.View):
+    @miru.button(label="Chicago", style=hikari.ButtonStyle.PRIMARY)
+    async def button1(self, button: miru.Button, ctx: miru.ViewContext) -> None:
+        await handle_role(ctx, 1119033975680270346, "Chicago")
+
+
+async def handle_role(ctx, role_id, job_title):
+    user = ctx.author
+    user_id = ctx.author.id
+    guild_id = ctx.guild_id
+    member = await bot.rest.fetch_member(guild_id, user_id)
+    roles = await member.fetch_roles()
+    if role_id in [role.id for role in roles]: # checks if user already has role, and remove if they do
+        await member.remove_role(role_id)
+        print(f"{user} removed role")
+        return
     
+    await member.add_role(role_id)
+    print(f"{user} chose {job_title}!")
 
 
-
-# Create an instance of our bot. This doesn't need to be a GatewayBot,
-# but needs to implement RESTAware, CacheAware, and EventManagerAware.
 miru.install(bot) # Start miru
-# This function must be called on startup, otherwise you cannot instantiate views
+
 
 @bot.listen()
 async def buttons(event: hikari.GuildMessageCreateEvent) -> None:
@@ -63,16 +60,20 @@ async def buttons(event: hikari.GuildMessageCreateEvent) -> None:
     if not me.id in event.message.user_mentions_ids:
         return
     
-    view = BasicView(timeout=None)  # Create a new view
-    embed = Embed(title="Greeting", description="Hello miru!", color=0x00ff00)  # Green
-    message = await event.message.respond(embed=embed, components=view)
-    await view.start(message)  # Start listening for interactions
-    await view.wait() # Optionally, wait until the view times out or gets stopped
-    await event.message.respond("Goodbye!")
+    fieldView = FieldView(timeout=None)  # Create a new view
+    locationView = LocationView(timeout=None)
+    fieldEmbed = Embed(title="Field", description="Pick a Job!\nSelect Job Again To Remove Role", color=0x00ff00)  # Green
+    locationEmbed = Embed(title="Location", description="Pick a location!")
+    fieldMessage = await event.message.respond(embed=fieldEmbed, components=fieldView)
+    locationMessage = await event.message.respond(embed=locationEmbed, components=locationView)
+    await fieldView.start(fieldMessage)  # Start listening for interactions
+    await locationView.start(locationMessage)
+
 
 @bot.listen(hikari.StartingEvent)
 async def on_started(event: hikari.StartingEvent) -> None:
     print("Bot is now ready.")
+
 
 @bot.command
 @lightbulb.command("ping", "checks if the bots alive")
@@ -80,8 +81,6 @@ async def on_started(event: hikari.StartingEvent) -> None:
 async def ping(ctx):
     await ctx.respond("here we go!")
 
-# response = requests.get(url, headers=headers, params=querystring)
-# gets this and prints out something
 
 @bot.command
 @lightbulb.option("query", "thing you wanna search for idk")
@@ -94,6 +93,6 @@ async def response(ctx):
         json.dump(data, outfile)
     await ctx.respond("responded")
 
-# helps to run the bot
+
+# Runs the bot
 bot.run()
-#anything after this would not be ran until 
