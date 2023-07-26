47d35ba3e67aec31c85cfc7f443b3933ccdabb49
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-07-26 10:32:26 -0500

    rand

--- leetcode.py
+++ leetcode.py
@@ -1,67 +1,57 @@
-from bs4 import BeautifulSoup
-# from requests_html import HTMLSession
+from pprint import pp
+import re
 import requests
-from playwright.sync_api import sync_playwright
+import json
+import random
 
-# proxy = "34.87.130.147:8080"
+# url to JS file of neetcode script that populates data
 
-url = "https://neetcode.io/practice"
-url2 = "https://leetcode.com/problems/substring-with-largest-variance/"
-url3 = "https://neetcode.io/main.e9d3b0a379eb0fd4.js"
+class Leetcode:
 
-# session = HTMLSession()
+    def __init__(self):
+        self.url = "https://neetcode.io/main.e9d3b0a379eb0fd4.js"
+        self.rand = random.randint(0, 74)
+        self.data_json = self.parse_data(self.url)
 
-try:
-    response1 = requests.get('https://httpbin.org/ip', timeout=3)
-    print(response1.json())
-except Exception as e:
-    print(e)
+    def parse_data(self, url) -> json:
 
-try:
-    # with sync_playwright() as p:
-    #     browser = p.chromium.launch()
-    #     page = browser.new_page()
-    #     page.goto(url)
-    #     # with open('output4.html', 'w') as f:
-    #     #     f.write(page.content())
-    #     accordion_button = page.locator("button.accordion").nth(1)
-    #     accordion_button.click()
-    #     row_locator = page.locator("tr").filter(has_text="3Sum")
-    #     # row_locator.filter(has_text="3Sum")
-    #     print(row_locator.get_attribute("class"))
-    #     buttons = row_locator.locator('a#discord-gray').first
-    #     print(buttons)
-    #     buttons.click()
-    #     # with open('output5.html', 'w') as f:
-    #     #     f.write(page.content())
-    #     modal = page.locator('.modal.is-active.ng-star-inserted.dialog-open')
-    #     print(modal)
-    #     youtube = modal.get_by_text('View on Youtube')
-    #     print(youtube.get_attribute('href'))
-    #     browser.close()
+        r = requests.get(url, timeout=3)
+        js_code = r.text
 
-        with sync_playwright() as p:
-            browser = p.chromium.launch()
-            page= browser.new_page()
-            page.goto(url)
-            for category in page.locator("div.accordion-container").all():
-                print('category', category.locator("p").first.inner_text())
-                category_button = category.locator("button").first
-                # category_button.click()
-                rows = category.locator("tbody").locator("tr.ng-star-inserted").all()
-                print([row.locator("a.table-text").inner_text() for row in rows])
-                for row in rows:
-                    title_tag = row.locator("td a.table-text").first
-                    title = title_tag.inner_text()
-                    print("title: ", title)
-                    buttons = row.locator('a#discord-gray').first
-                    buttons.click()
-                    modal = page.locator('.modal.is-active.ng-star-inserted.dialog-open')
-                    youtube = modal.get_by_text('View on Youtube')
-                    print(youtube.get_attribute('href'))
-                    close = modal.get_by_role("button")
-                    print("close", close.locator("b").inner_text())
-                    close.click()
-            browser.close()
-except Exception as e:
-    print("timed out\n", e, "\n")
+        # parses the JS file to get the array lists
+        matches = re.findall(r'C=(\[.*?\])', js_code, re.DOTALL)
+        for match in matches:
+            # print(match)
+            if match == '[]' or not match:
+                continue
+            if match == "['*']" or match == '["*"]':
+                continue
+
+            # converts the string into JSON
+            match = match.replace('!0', 'true').replace('!1', 'false')
+
+            # More specific matching for key-value pairs
+            match = re.sub(r'(?<={|,)\s*([\'"]?\w+[\'"]?)\s*:', r'"\1":', match)
+
+            json_objects = json.loads(match)
+            json_objects = [obj for obj in json_objects if obj.get('blind75') is True]
+
+            return json_objects
+        return None
+
+    def get_youtube_url(self) -> str:
+        endpoint = self.data_json[self.rand].get("video")
+        return f'https://www.youtube.com/watch?v={endpoint}'
+    
+    def get_solution_url(self) -> str:
+        endpoint = self.data_json[self.rand].get("code")
+        return f'https://github.com/neetcode-gh/leetcode/blob/main/python/{endpoint}.py'
+    
+    def get_leetcode_url(self) -> str:
+        endpoint = self.data_json[self.rand].get("link")
+        return f'https://leetcode.com/problems/{endpoint}'
+        
+ 
+if __name__ == '__main__':
+    l = Leetcode()
+    print(l.get_leetcode_url(), l.get_solution_url(), l.get_youtube_url())
